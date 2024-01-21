0e8f2f53d4b0b9c01c6ea5c0922d98ea09068dc0
sahilraut59 <sahilraut59@gmail.com>
2024-01-21 16:17:38 +0530

    Added Toggle like api

--- src/index.js
+++ src/index.js
@@ -17,12 +17,12 @@ app.listen(3000, async () => {
 
   await connect();
   console.log("Connnected to DB");
-  const userRepo = new UserRepository();
-  const tweetRepo = new TweetRepository();
-  const tweets = await tweetRepo.getAll(0, 10);
+  // const userRepo = new UserRepository();
+  // const tweetRepo = new TweetRepository();
+  // const tweets = await tweetRepo.getAll(0, 10);
 
-  const users = await userRepo.getAll();
+  // const users = await userRepo.getAll();
 
-  const likeService = new LikeService();
-  await likeService.toggleLike(tweets[0].id, "Tweet", users[0].id);
+  // const likeService = new LikeService();
+  // await likeService.toggleLike(tweets[0].id, "Tweet", users[0].id);
 });
