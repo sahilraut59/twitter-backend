b3086eaf2470a880c082743a468bb5e4fb2e72e1
sahilraut59 <sahilraut59@gmail.com>
2024-01-24 20:02:06 +0530

    Added passport auth

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -11,17 +11,37 @@ export const signup = async (req, res) => {
     });
 
     return res.status(201).json({
-        success:true,
-        data:response,
-        message:'Successfully created a user',
-        err:{}
-    })
+      success: true,
+      data: response,
+      message: "Successfully created a user",
+      err: {},
+    });
   } catch (error) {
     return res.status(500).json({
       success: false,
       data: {},
       err: error,
       message: "Failed to signup",
+    });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const token = await userService.signin(req.body);
+
+    return res.status(200).json({
+      success: true,
+      message: "successfully logged in",
+      data: token,
+      err: {},
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      data: {},
+      err: error,
+      message: "Something went wrong",
     });
   }
 };
